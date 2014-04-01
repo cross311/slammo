ab50354d963a5dcea2dd6a058fdd941a63da907f
Dan Hoizner <dan.hoizner@gmail.com>
2014-04-01 16:53:16 -0400

    adding bootstrap-sass gem and bootstrap to application.js and application.css.scss

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require ./angular-1.2.7/angular
 //= require ./angular-1.2.7/angular-route
 //= require ./angular-1.2.7/angular-resource
