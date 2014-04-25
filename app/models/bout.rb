578f6b0244d6265a64078b8bdf59ede0202b4d8c
Connor Ross <cross@mdsol.com>
2014-04-25 12:26:10 -0400

    adding in the check tomake sure more then two teams are not added to a bout

--- app/models/bout.rb
+++ app/models/bout.rb
@@ -2,6 +2,11 @@ class Bout < ActiveRecord::Base
   has_many :fantasy_bout_roostered_skaters
   has_many :fantasy_bout_scores
   has_many :skater_bout_scores
-  has_and_belongs_to_many :teams, -> { order("created_at ASC").limit(2) }
+  has_and_belongs_to_many :teams, -> { order("created_at ASC").limit(2) }, before_add: :check_only_two_teams
+  
+  def check_only_two_teams(new_team)
+    already_has_two_or_more_teams = teams.size >= 2
+    throw new Exception("Bout already has two teams. Cannot add team #{new_team.name}") if already_has_two_or_more_teams
+  end
   
 end
--- test/models/bout_test.rb
+++ test/models/bout_test.rb
@@ -4,4 +4,27 @@ class BoutTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "bouts can only have two teams" do
+    # Arrange
+    team1 = Team.build(:external_id => '1', :name => 'test1')
+    team2 = Team.build(:external_id => '2', :name => 'test2')
+    team3 = Team.build(:external_id => '3', :name => 'test3')
+    
+    bout = Bout.build(:external_id => '1')
+    
+    # Act
+      bout.teams << team1
+      bout.teams << team2
+      
+    try
+      bout.teams << team3
+    end
+    catch
+      return
+    end
+    
+    assert false
+  end
+  
 end
