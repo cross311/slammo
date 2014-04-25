6059788ae62cfb446aa68ce30c5a8e76dc67b27b
Connor Ross <cross@mdsol.com>
2014-04-25 07:48:04 -0400

    ran db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140425113138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bouts", force: true do |t|
+    t.string   "external_id"
+    t.datetime "bout_start"
+    t.datetime "rooster_close"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fantasy_bout_roostered_skaters", force: true do |t|
+    t.string   "position"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fantasy_bout_scores", force: true do |t|
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fantasy_managers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "fantasy_team_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "fantasy_team_skaters", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skater_bout_scores", force: true do |t|
+    t.integer  "jammer_points_scored"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skaters", force: true do |t|
+    t.string   "external_id"
+    t.string   "derby_name"
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "teams", force: true do |t|
+    t.string   "external_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
